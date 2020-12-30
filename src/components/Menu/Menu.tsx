e76972e67685349e3568061f984dd6cfb1a3aeb2
keith <kdang@quartermaster.house>
2020-12-30 15:18:25 -0500

    Form to add name

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPerson } from '../../actions'
 
-const Menu = () => {
+const Menu: React.FC = () => {
     const dispatch = useDispatch();
-    const handleClick = () => {
-        dispatch(addPerson('Yolo'))
+    const handleClick = (name: string) => {
+        dispatch(addPerson(name))
     }
-
+    let input: HTMLInputElement | null;
     return (
-        <div>
-            <button onClick={handleClick}>Add</button>
-
-        </div>
+        <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if(input != null){
+            handleClick(input.value)
+            input.value = "";
+          }
+        }}
+        >
+            <button>Add</button>
+            <input ref={(node) => (input = node)}/>
+        </form>
     )
 }
 
